8df188e6690a9a7b0252181df1a812183db80902
Furkan Ayilmaz <46174893+furkanayilmaz@users.noreply.github.com>
2021-02-16 10:08:45 -0700

    Update ClientRunner.java

--- src/com/message/messenger/ClientRunner.java
+++ src/com/message/messenger/ClientRunner.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class ClientRunner {
     public static void main(String[] args) {
         Client client;
-        client = new Client("192.168.0.4");
+        client = new Client("xxx.xxx.x.x");
         client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         client.startRunning();
 
